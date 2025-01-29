7fb515b760276ad0f4bb6687a467e91d5fad8607
gabrielluttjohann <gabrielcristianoluttjohann@gmail.com>
2025-01-29 18:22:08 -0300

    first commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,18 @@
+import logo from "@/assets/logo.png";
+
 export default function Header() {
-    return (
-        <div className="bg-white p-5"></div>
-    );
+  return (
+    <header className="bg-pale-green p-3 flex items-center justify-center">
+      <div className="container mx-auto text-white flex items-center justify-between">
+        <img className="object-fill w-32 h-auto" src={logo.src} alt="" />
+        <nav>
+          <ul className="flex gap-3">
+            <li className="">Quem Somos</li>
+            <li>Serviços</li>
+            <li>Fale Conosco</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
